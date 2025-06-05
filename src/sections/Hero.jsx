ca261513c638779f1e40a43d5758b5314b419776
ShivamKr-T <kumarshivam63834@gmail.com>
2025-06-05 12:25:47 +0530

    added style and animation

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,49 @@
+import Button from '../components/Button.jsx'
+import {words} from '../constants/index.js'
+
+const Hero = () => {
+  return (
+    <section id="hero" className='relative overflow-hidden'>
+        <div className='absolute top-0 left-0 z-10'>
+            <img src="/images/bg.png" alt="background" />
+        </div>
+
+        <div className='hero-layout'>
+            {/* {left:hero content} */}
+            <header className='flex flex-col justify-centre md:w-full w-screen md:px-20 px-5'>
+                <div className='flex flex-col gap-7'>
+                    <div className='hero-text'>
+                        {/* CHNANGE A/C TO YOURS */}
+                        <h1>Hi👋🏻,</h1>
+                        <h1>I am <span style={{color: '#ffd580'}}>Shivam </span>,</h1>
+                        <h1>A
+                            <span className='slide'>
+                                <span className='wrapper'>
+                                    {words.map((word)=>(
+                                        <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
+                                            <img
+                                                src={word.imgPath}
+                                                alt={word.text}
+                                                className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
+                                            />
+                                            <span>{word.text}</span>
+                                        </span>
+                                    ))}
+                                </span>
+                            </span>
+                        </h1>
+                    </div>
+                    <Button
+                        className="md:w-80 md:h-16 w-60 h-12"
+                        id="button"
+                        text="See my Work"
+                    />
+                </div>
+            </header>
+            {/* {right: 3d model} */}
+        </div>
+    </section>
+  )
+}
+
+export default Hero
