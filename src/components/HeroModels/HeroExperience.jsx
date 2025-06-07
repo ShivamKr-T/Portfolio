b82bbfcb52b9b6c0dce8f99db4b31ca8b69d1e34
ShivamKr-T <kumarshivam63834@gmail.com>
2025-06-07 17:04:19 +0530

    improved 3d-model and added projects

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -6,10 +6,10 @@ import { Cute } from './Cute';
 import Particles from './Particles';
 
 const HeroExperience = () => {
-    const isTablet=useMediaQuery({query:'max-width:1024px'});
-    const isMobile=useMediaQuery({query:'max-width:768px'})
+    const isTablet=useMediaQuery({query:'(max-width:1024px)'});
+    const isMobile=useMediaQuery({query:'(max-width:768px)'})
   return (
-    <Canvas camera={{position:[0,0,15],fov:45}}>
+    <Canvas camera={{position:[5,1,15],fov:45}}>
         <ambientLight intensity={0.2} color='#1a1a40'/>
         <directionalLight position={[5,5,5]} intensity={2}/>
 
@@ -24,7 +24,7 @@ const HeroExperience = () => {
 
         <Particles count={150}/>
         <group
-            scale={isMobile?0.7:1} 
+            scale={isMobile?1.2:1.5} 
             position={[0.5,0,0]}
         >
             <Cute/>
--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -44,27 +44,27 @@ const ShowcaseSection = () => {
                 {/* CHANGE ACCORDING TO YOURS */}
                 <div className='first-project-wrapper' ref={project1Ref}>
                     <div className='image-wrapper'>
-                        <img src="/images/project1.png" alt="Ryde" />
+                        <img src="/images/lms_2.png" alt="Ryde" />
                     </div>
                     <div className='text-content'>
-                        <h2>yaha apne project ka description</h2>
-                        <p>batao project ke baare ,me</p>
+                        <h2>Learning Management System</h2>
+                        <p>A web-based platform designed to streamline the delivery, tracking, and management of educational content. The system allows instructors to create and manage courses, upload learning materials, assign tasks, and evaluate performance, while students can access resources, track progress, and submit assignments, all in one centralized portal. The system supports user authentication, role-based access, and real-time notifications to enhance the learning experience.</p>
                     </div>
                 </div>
                 {/* RIGHT */}
                 <div className='project-list-wrapper overflow-hidden'>
                     <div className='project' ref={project2Ref}>
-                        <div className='image-wrapper bg-[#ffefdb]'>
-                            <img src="/images/project2.png" alt="Library Management" />
+                        <div className='image-wrapper bg-[#e3ffdb]'>
+                            <img src="/images/agent.png" alt="Library Management" />
                         </div>
-                        <h2>Library Management System no decritpon need</h2>
+                        <h2>CyberCraft — An AI-Driven SOC Analyst for Proactive Cyber Threat Detection</h2>
                     </div>
 
                     <div className='project' ref={project3Ref}>
                         <div className='image-wrapper bg-[#ffe7eb]'>
-                            <img src="/images/project2.png" alt="Library Management" />
+                            <img src="/images/phish.png" alt="Library Management" />
                         </div>
-                        <h2>Library Management System no decritpon need</h2>
+                        <h2>Phish Eye — An Intelligent Email Scanner for Accurate Phishing Detection</h2>
                     </div>
                 </div>
             </div>
